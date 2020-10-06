7a64c43cadc4aa48a47a6d26cbd79ac6334675cb
admin <admin@ETUDIANT.local>
2020-10-06 11:53:22 +0200

    add template and home view

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class HomeController.
+ *
+ * @method getRequest()
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     *
+     * @return Response
+     */
+    public function __invoke(): Response
+    {
+        return $this->render('default/home.html.twig');
+    }
+}
