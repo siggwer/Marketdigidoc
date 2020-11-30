755c970b969a9f93f021ec7216eea7e641ed268d
admin <admin@ETUDIANT.local>
2020-11-30 16:46:05 +0100

    update login view and update html to pdf functionality

--- src/Controller/DocumentToPdf.php
+++ src/Controller/DocumentToPdf.php
@@ -5,15 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Document;
 use Dompdf\Options;
 use Dompdf\Dompdf;
-
-
 
 /**
  * DocumentToPdf class
@@ -25,37 +20,39 @@ class DocumentToPdf extends AbstractController
     /**
     * @Route("/pdf/{slug}", name="document_to_pdf", methods={"GET"})
     *
-    * 
+    *
     *
     * @return void
     */
     public function actionToPdf(Document $document)
     {
         
-     // Configure Dompdf according to your needs
-     $pdfOptions = new Options();
-     $pdfOptions->set('defaultFont', 'Arial');
+      // Configure Dompdf according to your needs
+      //$pdfOptions = new Options();
+      //$pdfOptions->set('defaultFont', 'Arial');
      
-     // Instantiate Dompdf with our options
-     $dompdf = new Dompdf($pdfOptions);
-     
-     // Retrieve the HTML generated in our twig file
-     $html = $this->renderView('document/pdf.view.html.twig', [
-         'document' => $document
-     ]);
-     dd($html);
-     // Load HTML to Dompdf
-     $dompdf->loadHtml($html);
-     
-     // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
-     $dompdf->setPaper('A4', 'portrait');
+      // Instantiate Dompdf with our options
+      //$dompdf = new Dompdf($pdfOptions);
+      $dompdf = new Dompdf();
+      // Retrieve the HTML generated in our twig file
+      $html = $this->renderView('document/pdf.view.html.twig', [
+          'document' => $document
+      ]);
 
-     // Render the HTML as PDF
-     $dompdf->render();
+      $html .='<link type="text/css" href="C:\Users\admin.ETUDIANT\Desktop\Marketdigidoc\public\css\style.css" rel="stylesheet" />';
+      
+      // Load HTML to Dompdf
+      $dompdf->loadHtml($html);
+     
+      // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+      $dompdf->setPaper('A4', 'portrait');
 
-     // Output the generated PDF to Browser (inline view)
-     $dompdf->stream("mypdf.pdf", [
-         "Attachment" => false
-     ]);
+      // Render the HTML as PDF
+      $dompdf->render();
+
+      // Output the generated PDF to Browser (inline view)
+      $dompdf->stream("mypdf.pdf", [
+          "Attachment" => false
+      ]);
     }
 }
